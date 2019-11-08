8da6f85f9e41f4370a6caccb6aa4080e7241f6b6
Ranjith <ranju08134@gmail.com>
2019-11-08 21:32:03 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
