1211743990e4c3547bd9eefe671802ed0346a63e
Ranjith <ranju08134@gmail.com>
2019-11-19 18:43:14 +0530

    Updated

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,33 +7,37 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
-const Header = ({currentUser}) =>(
+
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
-      <Link className='logo-container 'to='/'>
-       <Logo className='logo'/>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
       </Link>
       <div className='options'>
-          <Link className='option' to='/shop'>
-              SHOP
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/shop'>
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className='option' onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className='option' to='/signin'>
+            SIGN IN
           </Link>
-          <Link className='option' to='/shop'>
-              CONTACT
-          </Link>
-          {
-            //   ternary operator to conditionally render component
-              currentUser?
-              <div className='option' onClick={() =>auth.signOut()}>SIGN OUT</div>
-              :
-              <Link className='option' to ='/signin'> SIGN IN</Link>
-          }
-          <CartIcon />
+        )}
+        <CartIcon />
       </div>
-      <CartDropdown />
+      {hidden ? null : <CartDropdown />}
     </div>
-);
-
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+  );
+  
+  const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
   });
   
   export default connect(mapStateToProps)(Header);
