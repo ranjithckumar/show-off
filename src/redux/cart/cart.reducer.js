c294e054d482e6cd18018fadadc48227868dc405
Ranjith <ranju08134@gmail.com>
2019-11-22 18:56:19 +0530

    Updated

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,8 +1,9 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
+
 const INITIAL_STATE = {
   hidden: true,
-  cartItems:[]
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -12,12 +13,17 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       };
-      case CartActionTypes.ADD_ITEM:
-          return {
-              ...state,
-              cartItems : addItemToCart(state.cartItems, action.payload)
-          };
-      case CartActionTypes.CLEAR_ITEM_FROM_CART:
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
       return {
         ...state,
         cartItems: state.cartItems.filter(
