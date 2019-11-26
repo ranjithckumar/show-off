4c10c73cd2605a711b9cf45eb6ce67b3e6db64e1
Ranjith <ranju08134@gmail.com>
2019-11-26 19:21:34 +0530

    Added react persist feature

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore,applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 import routeReducer from './route-reducer';
 
 // adding middleware i.e middlewares
 
 const middlewares = [logger];
-const store = createStore( routeReducer , applyMiddleware(...middlewares));
+export const store = createStore( routeReducer , applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
 
-export default store;
+export default {store, persistor};
