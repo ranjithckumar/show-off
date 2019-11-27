c0a3df9a2b2bc8f52cf528f8b256f2e1c73d5445
Ranjith <ranju08134@gmail.com>
2019-11-27 13:16:04 +0530

    Updated

--- src/pages/shop/shop.component .jsx
+++ src/pages/shop/shop.component .jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Route } from 'react-router-dom';
+import CategoryPage from '../category/category.component';
 // import { connect } from 'react-redux';
 // import {createStructuredSelector}  from 'reselect';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 // import {selectCollections} from  '../../redux/shop/shop.selectors';
 
-const ShopPage =({ collections })=>(
+const ShopPage =({match})=>(
+ 
   <div className='shop-page'>
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component = {CollectionsOverview} /> 
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage}/>
 </div>
 );
 
